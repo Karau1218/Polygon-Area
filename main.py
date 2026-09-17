137da6f445b047770df6ba3b75360a2b8cd5b855
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-09-16 23:08:22 -0700

    add Square class with methods for area, perimeter, and more

--- main.py
+++ main.py
@@ -30,3 +30,20 @@ class Rectangle:
 
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.set_side(width)
+
+    def set_height(self, height):
+        self.set_side(height)
+
+    def __str__(self):
+        return f"Square(side={self.width})"
